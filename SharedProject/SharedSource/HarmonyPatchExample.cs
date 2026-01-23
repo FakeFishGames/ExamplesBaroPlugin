82c7ec8ed30cdc926f60bf3cd826519d09e5a072
Evil Factory <36804725+evilfactory@users.noreply.github.com>
2026-01-23 20:59:18 -0300

    Add basic hook and harmony example

---
+++ SharedProject/SharedSource/HarmonyPatchExample.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Barotrauma;
+using Microsoft.Xna.Framework;
+
+namespace Examples;
+
+[HarmonyPatch]
+public static class HarmonyPatchExamples
+{
+#if CLIENT
+    [HarmonyPatch(typeof(GUITextBlock), nameof(GUITextBlock.TextColor), MethodType.Getter), HarmonyPostfix]
+    private static void GUITextBlock_GetTextColor_Post(ref Color __result)
+    {
+        __result = Color.Red;
+    }
+#endif
+}
