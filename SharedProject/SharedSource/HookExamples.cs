e235447c783c2ff4f1a4812eadb68c2af3697ee8
Evil Factory <36804725+evilfactory@users.noreply.github.com>
2026-01-23 20:59:18 -0300

    Add basic hook and harmony example

---
+++ SharedProject/SharedSource/HookExamples.cs
@@ -0,0 +1,24 @@
+﻿using Barotrauma;
+using Barotrauma.Plugins;
+using Microsoft.Xna.Framework;
+
+namespace Examples;
+
+public static class HookExamples
+{
+    public static void RegisterHooks()
+    {
+        Plugin.HookService.RegisterHook<PluginRoundStartedDelegate>(OnRoundStarted);
+        Plugin.HookService.RegisterHook<PluginRoundEndedDelegate>(OnRoundEnded);
+    }
+
+    private static void OnRoundStarted(GameSession gameSession)
+    {
+        Plugin.DebugConsole.NewMessage("Round started!", Color.Red);
+    }
+
+    private static void OnRoundEnded(GameSession gameSession)
+    {
+        Plugin.DebugConsole.NewMessage("Round ended!", Color.Red);
+    }
+}
--- SharedProject/SharedSource/Plugin.cs
+++ SharedProject/SharedSource/Plugin.cs
@@ -1,5 +1,6 @@
 ﻿using Barotrauma;
 using Barotrauma.Plugins;
+using HarmonyLib;
 using Microsoft.Xna.Framework;
 
 namespace Examples;
@@ -9,21 +10,27 @@ public partial class Plugin : IBarotraumaPlugin
     public static readonly IDebugConsole DebugConsole = PluginServiceProvider.GetService<IDebugConsole>();
     public static readonly ISettingsService SettingsService = PluginServiceProvider.GetService<ISettingsService>();
     public static readonly IItemComponentRegistrar ItemComponentRegistrar = PluginServiceProvider.GetService<IItemComponentRegistrar>();
+    public static readonly ISimpleHookService HookService = PluginServiceProvider.GetService<ISimpleHookService>();
+    public static readonly IHarmonyProvider HarmonyProvider = PluginServiceProvider.GetService<IHarmonyProvider>();
 
     public void Init()
     {
-        DebugConsole.NewMessage("Plugin loaded", Color.Lime);
-
-        ItemComponentRegistrar.RegisterAllItemComponents();
+        DebugConsole.NewMessage("Plugin example loaded", Color.Lime);
 
         InitProjectSpecific();
+
+        ItemComponentRegistrar.RegisterAllItemComponents();
+        HarmonyProvider.PatchAll();
+
+        HookExamples.RegisterHooks();
     }
+
 
     public partial void InitProjectSpecific();
 
     public void Dispose() 
     { 
-        DebugConsole.NewMessage("Plugin unloaded", Color.Red); 
+        DebugConsole.NewMessage("Plugin example unloaded", Color.Red); 
     }
 
     public void OnContentLoaded()
